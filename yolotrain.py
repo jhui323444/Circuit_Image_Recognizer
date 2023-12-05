b5853aa7230389c518e6d0cb8ced8540188a87dc
jhui323444 <jhui323444@gmail.com>
2023-12-05 01:20:17 -0800

    completed training on 100 epochs, written to train8

--- yolotrain.py
+++ yolotrain.py
@@ -4,5 +4,5 @@ model = YOLO('yolov8n.yaml')
 model = YOLO('yolov8n.pt')
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')
 
-results = model.train(data='config.yaml', batch=4, epochs=10)
+results = model.train(data='config.yaml', batch=6, epochs=100)
 
