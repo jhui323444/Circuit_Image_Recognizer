1356d572feff603eb99a4ffc121773d90da68af7
jhui323444 <jhui323444@gmail.com>
2024-03-14 21:40:48 -0700

    trained dataset some more and cleaned up the gitignore

--- train/yoloresume.py
+++ train/yoloresume.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('./runs/detect/train8/weights/last.pt')
+model = YOLO('../runs/detect/train82/weights/last.pt')
 
 results = model.train(data='config.yaml', batch=6, epochs=100, resume=True)
